5f53c564fecc908a219bb7c2593a8ef08272f129
Alexander Ryzhov <aryzhov84@gmail.com>
2022-08-06 16:27:59 +0300

    add T5

---
+++ T5/Program.cs
@@ -0,0 +1,29 @@
+﻿// Запись  массива(любого размера) и вывода его элементов(любого количества) 
+// с пощью гениратора псевдослучайных чисел-new Random().Next() и методов-void.
+
+void FillArray(int[] collection) // 1 метод вычисления массива
+{
+    int Length = collection.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        collection[index] = new Random().Next(1, 300);
+        index++;
+    }
+}
+
+void PrintArray(int[] col) //2 метод.Печать void-метод который ничего не возвращает,return с ним не используют
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[1];
+
+FillArray(array);
+PrintArray(array);
